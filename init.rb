79f7f374cb066ed5cdde7b82b344b7bf1a840201
maimai77 <kohta0707@gmail.com>
2024-03-18 14:21:04 +0900

    Updated require styles

--- init.rb
+++ init.rb
@@ -2,4 +2,6 @@ Redmine::Plugin.register :api_without_key do
   name 'api_without_key plugin'
 end
 
-require File.expand_path('lib/api_without_key/application_helper', __dir__)
+Pathname.new(__dir__).glob('lib/**/*.rb').sort.each do |path|
+  require path
+end
